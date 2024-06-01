e85a6f2b286e3e10613d79774a10ac4593a3795d
Kravchenko Konstantin <k.kravchenko@inits.com.ua>
2024-06-01 19:43:52 +0300

    Add copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const dotenv = require('dotenv').config({ path: __dirname + '/.env' })
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
